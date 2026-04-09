052b1e4cd1f127b737aa06937cf3de8b1eacbbbf
pokustone <61164170+pokustone@users.noreply.github.com>
2026-04-09 17:59:22 +0200

    Add files via upload

---
+++ src/lib/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCsAkdDE57cNjTgeX7b_AWzabBLB2V7eOo",
+  authDomain: "swampsound-system.firebaseapp.com",
+  projectId: "swampsound-system",
+  storageBucket: "swampsound-system.firebasestorage.app",
+  messagingSenderId: "165964578189",
+  appId: "1:165964578189:web:f07915f140d2cfdd3991c4"
+};
