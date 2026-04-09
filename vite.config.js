03a376296283303b207499f070a18548e5246d33
pokustone <nemec.jakub@gmail.com>
2026-04-09 18:22:40 +0200

    SwampSound-Platform-v1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Změň na '/nazev-repa/' pokud se repo jmenuje jinak
-  base: '/swampsound-system/',
+  base: '/swampsound-platform/',
 })
